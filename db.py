08aef44173596398d000ee4579c4690a91497a02
musayevjahongir <musayevjahongir5051@gmail.com>
2023-12-19 20:56:48 +0500

    tinydb

--- db.py
+++ db.py
@@ -1,10 +1,26 @@
 from tinydb import TinyDB
-from tinydb.database import Document
+import requests
+
+def user():
+
+    response=requests.get('https://randomuser.me/api/').json()
+    return response["results"][0]
 
 
 db = TinyDB('db.json', indent=4)
 
-users = db.table(name='users')
+male = db.table(name='male')
+female=db.table(name="female")
 
-# print(users.all())
-print(users.get(doc_id=31))
+while True:
+    users=user()
+    if users['gender']=="male":
+        if len(male)<=30:
+            male.insert(users)
+    elif users['gender']=="female":
+        if len(female)<=30:
+            female.insert(users)
+        else:
+            break
+
+
